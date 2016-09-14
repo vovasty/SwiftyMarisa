d7d750f7b1f0ff69ff44d17336b607e0f7e83e09
Vladimir Solomenchuk <vlad@aramzamzam.net>
2016-09-14 15:39:57 -0700

    lint

--- SwiftyMarisa/Marisa.swift
+++ SwiftyMarisa/Marisa.swift
@@ -3,53 +3,63 @@
 //  Copyright (c) 2016, Vladimir Solomenchuk
 //  All rights reserved.
 //
-//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
-//  following conditions are met:
+//  Redistribution and use in source and binary forms, with or without modification, are permitted
+//  provided that the following conditions are met:
 //
-//  - Redistributions of source code must retain the above copyright notice, this list of conditions and the following
-//  disclaimer.
-//  - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+//  - Redistributions of source code must retain the above copyright notice, this list of conditions
+//  and the following disclaimer.
+//  - Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the following
 //  disclaimer in the documentation and/or other materials provided with the distribution.
 //
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-//  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF 
-//  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
-//  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
-//  OF THE POSSIBILITY OF SUCH DAMAGE.
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES,
+//  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+//  PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+//  GOODS OR SERVICES; LOSS OF
+//  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT
+//  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+//  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Foundation
 
-open class SearchResults : Sequence {
+open class SearchResults: Sequence {
     fileprivate let context: OpaquePointer
     fileprivate let query: String
     fileprivate var searchContext: OpaquePointer? = nil
     fileprivate let type: MarisaSearchType
-    
+
     init(context: OpaquePointer, query: String, type: MarisaSearchType) {
         self.context = context
         self.query = query
         self.type = type
     }
-    
+
     open func makeIterator() -> AnyIterator<String> {
         var buf: UnsafeMutablePointer<CChar>? = nil
         var len: Int = 0
-        
+
         let pointer = UnsafeMutablePointer<CChar>(mutating: (self.query as NSString).utf8String)
-        
-        searchContext = marisa_search(context, pointer, self.type);
-        
-        return AnyIterator<String>{
+
+        searchContext = marisa_search(context, pointer, self.type)
+
+        return AnyIterator<String> {
             guard marisa_search_next(self.searchContext!, &buf, &len) == 1 else { return nil }
-            return String(bytesNoCopy: buf!, length: len, encoding: String.Encoding.utf8, freeWhenDone: false)
+
+            return String(bytesNoCopy: buf!,
+                               length: len,
+                             encoding: String.Encoding.utf8,
+                         freeWhenDone: false)
         }
     }
-    
+
     deinit {
         if searchContext != nil {
-            marisa_delete_search_context(searchContext!);
+            marisa_delete_search_context(searchContext!)
         }
     }
 }
@@ -57,14 +67,14 @@ open class SearchResults : Sequence {
 
 open class Marisa {
     fileprivate var context = marisa_create_context()
-    fileprivate var searchCallback: ((String)->Bool)!
-    
+    fileprivate var searchCallback: ((String) -> Bool)!
+
     public init() {}
-    
+
     /**
      Builds dictionary.
      - parameter builder: Closure with builder parameter. Builder accepts string parameter.
-     
+
      ```
      ...
      trie.build { (builder) -> Void in
@@ -101,7 +111,7 @@ open class Marisa {
     open func lookup(_ query: String) -> Bool {
         return marisa_lookup(context, query) == 1
     }
-    
+
     /**
      Saves dictionary into a file.
      - parameter path: Path to a file.
@@ -117,7 +127,7 @@ open class Marisa {
     open func load(_ path: String) {
         marisa_load(context, path)
     }
-    
+
     deinit {
         marisa_delete_context(context)
     }
